f672b37ce3582080f2ab7d258901dd4a82618cf6
simonescoca <simone.scoca@icloud.com>
2023-05-16 14:33:44 +0200

    added bonus 1

--- BONUS/bonus.js
+++ BONUS/bonus.js
@@ -1,36 +1,34 @@
-// Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-// Consigli del giorno:
-// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 // BONUS 1:
 // Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 // BONUS 2:
 // Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
+const ulElement = document.querySelector ('ul')
+
 for (let i = 1 ; i < 101 ; i++) {
+
+    const li = document.createElement ('li')
+
     if (i % 3 === 0 && i % 5 !== 0) {
-        console.log ("Fizz")
+        li.append ("Fizz")
+        ulElement.appendChild (li)
     } else if (i % 5 === 0 && i % 3 !== 0) {
-        console.log ("Buzz")
+        li.append ("Buzz")
+        ulElement.appendChild (li)
     } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log ("FizzBuzz")
+        li.append ("FizzBuzz")
+        ulElement.appendChild (li)
     } else {
-        console.log (i)
+        li.append (i)
+        ulElement.appendChild (li)
     }
 }
 
 
 // CON L'ELEMENT
 
-const ulElement = document.querySelector ('ul')
 
 for (let i = 1 ; i < 6 ; i++ ) {
-    const li = document.createElement ('li')
     li.append ('Elemento Nº' + i)
     ulElement.appendChild (li)
 }
