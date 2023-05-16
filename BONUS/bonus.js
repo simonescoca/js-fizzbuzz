ff21537e6acdbdf842ea1000d723706a19f6362e
simonescoca <simone.scoca@icloud.com>
2023-05-16 14:50:10 +0200

    added bonus 2

--- BONUS/bonus.js
+++ BONUS/bonus.js
@@ -3,32 +3,26 @@
 // BONUS 2:
 // Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
-const ulElement = document.querySelector ('ul')
+const ulElement = document.querySelector ("ul")
 
 for (let i = 1 ; i < 101 ; i++) {
 
-    const li = document.createElement ('li')
+    const li = document.createElement ("li")
 
     if (i % 3 === 0 && i % 5 !== 0) {
         li.append ("Fizz")
+        li.classList.add ("fizz")
         ulElement.appendChild (li)
     } else if (i % 5 === 0 && i % 3 !== 0) {
         li.append ("Buzz")
+        li.classList.add ("buzz")
         ulElement.appendChild (li)
     } else if (i % 3 === 0 && i % 5 === 0) {
         li.append ("FizzBuzz")
+        li.classList.add ("fizzbuzz")
         ulElement.appendChild (li)
     } else {
         li.append (i)
         ulElement.appendChild (li)
     }
-}
-
-
-// CON L'ELEMENT
-
-
-for (let i = 1 ; i < 6 ; i++ ) {
-    li.append ('Elemento Nº' + i)
-    ulElement.appendChild (li)
 }
